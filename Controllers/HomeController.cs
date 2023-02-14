609bd7383d5b011a61956e92855c7d4b7bba7602
btpxii <btpaul12@gmail.com>
2023-02-13 23:30:24 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission6Assignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View("MyPodcasts");
+        }
+
+        [HttpGet]
+        public IActionResult EnterMovie()
+        {
+            return View("EnterMovie");
+        }
+
+        [HttpPost]
+        public IActionResult EnterMovie(ApplicationResponse ar)
+        {
+            return View("Confirmation", ar);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+    }
+}
---
+++ Models/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6Assignment.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext (DbContextOptions<MovieContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<ApplicationResponse> responses { get; set; }
+    }
+}
